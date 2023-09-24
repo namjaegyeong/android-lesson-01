27f243af8d1e36c01bbb00ad7c311228d8932ff6
namjaegyeong <fb_olddog@naver.com>
2023-09-24 23:01:24 +0900

    Feat: 다운로드 url 추가

--- src/main/java/kr/easw/lesson01/controller/rest/AWSController.java
+++ src/main/java/kr/easw/lesson01/controller/rest/AWSController.java
@@ -1,13 +1,18 @@
 package kr.easw.lesson01.controller.rest;
 
 import kr.easw.lesson01.model.dto.AWSKeyDto;
-import kr.easw.lesson01.model.dto.UploadFileDto;
 import kr.easw.lesson01.service.AWSService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @RestController
@@ -32,6 +37,20 @@ public class AWSController {
         return awsController.getFileList();
     }
 
+    @GetMapping("/download/{file}")
+    public ResponseEntity<byte[]> onDownload(
+            @PathVariable("file") String fileName
+    ) {
+        byte[] bytes = awsController.getS3Object(fileName);
+        String downloadFileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
+
+        return new ResponseEntity<>(
+                bytes,
+                getHeaders(bytes, downloadFileName),
+                HttpStatus.OK
+        );
+    }
+
     @PostMapping("/upload")
     private ModelAndView onUpload(@RequestParam MultipartFile file) {
         try {
@@ -41,5 +60,16 @@ public class AWSController {
             ex.printStackTrace();
             return new ModelAndView("redirect:/server-error?errorStatus=" + ex.getMessage());
         }
+    }
+
+    private HttpHeaders getHeaders(
+            byte[] bytes,
+            String fileName
+    ) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentLength(bytes.length);
+        headers.setContentDispositionFormData("attachment", fileName);
+        return headers;
     }
 }
